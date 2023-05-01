40347bf9bcc7cab152c31fbcb95b79da2f1761e1
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2023-05-01 13:19:44 +0530

    commit#5

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
   const [min, setMin] = useState(0);
   const [hour, setHour] = useState(0);
   const [pause, setPause] = useState(false);
+  const [start, setStart] = useState(true);
 
   const tick = () => {
     setCount(count + 1);
   }
 
   useEffect(() => {
-    if (!pause) {
+    if (!start && !pause) {
       const interval = setInterval(tick, 1000);
       return () => {
         clearInterval(interval);
@@ -21,17 +22,22 @@ function App() {
       }
     }
     // eslint-disable-next-line
-  }, [count, min, hour, pause]);
+  }, [count, min, hour, pause, start]);
 
   const reset = () => {
     setCount(0);
     setMin(0);
     setHour(0);
     setPause(false);
+    setStart(true);
   }
 
   const stop = () => {
     setPause(!pause);
+  }
+
+  const startTimer = () => {
+    setStart(false);
   }
 
   return (
@@ -40,10 +46,16 @@ function App() {
         <div className='row'>
           <h1 className='pb-4'>{String(hour).padStart(2, 0)} : {String(min).padStart(2, 0)} : {String(count % 60).padStart(2, 0)}</h1>
           <div>
-            <button onClick={reset}>Reset</button>
+            {
+              start
+                ?
+                <button onClick={startTimer}>Start</button>
+                :
+                <button onClick={reset}>Reset</button>
+            }
             <button onClick={stop}>
               {
-                pause ? <span>Resume</span> : <span>Pause</span>
+                pause && !start ? <span>Resume</span> : <span>Pause</span>
               }
             </button>
           </div>
